a2751651f35b81299b5329b6ce96b47a6f70b30e
hsperez6 <118867950+hsperez6@users.noreply.github.com>
2024-08-25 13:20:47 -0700

    Creating the App Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,37 @@
-import { createRoot } from  'react-dom/client';
-
-/** Example of JavaScript Expression
- 
-  const desc = "I'm reviewing JSX and React because of Unit 10"
-  const myTitleId = "main-title"
-  const name = "Laura"
-
-  const element = (
-    <>
-      <h1 id={myTitleId}>{name}'s first React Element!</h1>
-      <p className='main-desc'>{desc}</p>
-      <input value={10 * 20}/>
-    </>
-  );
-
-*/
+import { createRoot } from "react-dom/client";
 
 const Header = () => (
   <header>
     <h1>Grocery List</h1>
-    <span className='total-items'>Items: 1</span>
+    <span className="total-items">Items: 1</span>
   </header>
 );
 
-const root = createRoot(document.getElementById('root'));
-root.render(<Header />);
+const Item = () => (
+  <div className="item">
+    <button className="remove-item" />
+    <span className="item-name">Apples</span>
+    <Counter />
+  </div>
+);
+
+const Counter = () => (
+  <div className="quantity">
+    <span className="qty-label">QTY</span>
+    <button className="increment">+</button>
+    <button className="decrement">-</button>
+    <span className="quantity-amount">15</span>
+  </div>
+);
+
+const App = () => (
+  <div className="grocery-list">
+    <Header />
+
+    {/* Grocery List */}
+    <Item />
+  </div>
+);
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
