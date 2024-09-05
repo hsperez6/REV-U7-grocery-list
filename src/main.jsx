85f67ca897436a6c1dcfe72b50ae6351e088e556
hsperez6 <118867950+hsperez6@users.noreply.github.com>
2024-09-05 13:15:35 -0700

    Made App more Dynamic
    Added items array
    iterated through array using map() and rendered multiple <Item> components.

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,67 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-const Header = ({title, itemTotal}) => (
+const items = [
+  {
+    name: "Apples",
+    quantity: 5,
+    id: 1,
+  },
+  {
+    name: "Bananas",
+    quantity: 7,
+    id: 2,
+  },
+  {
+    name: "Pasta",
+    quantity: 1,
+    id: 3,
+  },
+  {
+    name: "Cookies",
+    quantity: 12,
+    id: 4,
+  },
+];
+
+const Header = ({ title, itemTotal }) => (
   <header>
     <h1>{title}</h1>
-    <span className="total-items">Items: {itemTotal(n => n + 1)}</span>
+    <span className="total-items">Items: {itemTotal}</span>
   </header>
 );
 
-const Item = () => (
+const Item = ({ name, quantity }) => (
   <div className="item">
     <button className="remove-item" />
-    <span className="item-name">Apples</span>
-    <Counter />
+    <span className="item-name">{name}</span>
+    <Counter quantity={quantity} />
   </div>
 );
 
-const Counter = () => (
+const Counter = ({ quantity }) => (
   <div className="quantity">
     <span className="qty-label">QTY</span>
     <button className="increment">+</button>
     <button className="decrement">-</button>
-    <span className="quantity-amount">15</span>
+    <span className="quantity-amount">{quantity}</span>
   </div>
 );
 
-const App = () => (
+const App = ({ initialList }) => (
   <div className="grocery-list">
-    <Header  
-	    title='Grocery List'
-	    itemTotal={10}
-	  />
+    <Header title="Grocery List" itemTotal={initialList.length} />
 
     {/* Grocery List */}
-    <Item />
+    {initialList.map((item) => (
+      <Item name={item.name} quantity={item.quantity} key={item.id} />
+    ))}
   </div>
 );
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <App initialList={items} />
   </StrictMode>
 );
