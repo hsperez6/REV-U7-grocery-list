380e2a6b53744b747247b0c393623e9eb957367a
hsperez6 <118867950+hsperez6@users.noreply.github.com>
2024-09-12 11:39:45 -0700

    Update main.jsx
    Using updater function to update state based on previous state

--- src/main.jsx
+++ src/main.jsx
@@ -44,12 +44,12 @@ const Counter = () => {
   const [quantity, setQuantity] = useState(0);
 
   const incrementQuantity = () => {
-    setQuantity( quantity + 1 );
+    setQuantity( prevQuantity => prevQuantity + 1 );
   };
 
   const decrementQuantity = () => {
     if (quantity > 0) {
-      setQuantity( quantity - 1 );
+      setQuantity( prevQuantity => prevQuantity - 1 );
     };
   };
 
