744ca83951c0881795d066ed31c10bd6663c4834
hsperez6 <hsperez@gmail.com>
2024-09-12 10:25:54 -0700

    Update main.jsx
    Added incrementQuantity and decrementQuantity functions to onClick

--- src/main.jsx
+++ src/main.jsx
@@ -44,14 +44,20 @@ const Counter = () => {
   const [quantity, setQuantity] = useState(0);
 
   const incrementQuantity = () => {
-    console.log('Hello from inside incrementQuantity');
+    setQuantity( quantity + 1 );
+  };
+
+  const decrementQuantity = () => {
+    if (quantity > 0) {
+      setQuantity( quantity - 1 );
+    };
   };
 
   return (
     <div className="quantity">
       <span className="qty-label">QTY</span>
       <button className="increment" onClick={incrementQuantity}>+</button>
-      <button className="decrement">-</button>
+      <button className="decrement" onClick={decrementQuantity}>-</button>
       <span className="quantity-amount">{quantity}</span>
     </div>
   );
